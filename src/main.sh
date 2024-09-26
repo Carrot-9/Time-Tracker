3937c80141aa1ca0a5b9fd60d9fb52fd9c5eca39
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-26 19:14:41 -0400

    checks file duration time

--- src/main.sh
+++ src/main.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "Hello World"
+
+start=$SECONDS
+
+echo "What is 2 + 2 ?:"
+read question
+
+if [[ $question = 4 ]]; then
+
+    echo "Correct!"
+else
+
+    echo "Incorrect!"
+fi
+
+duration=$(( SECONDS - start ))
+echo "Duration: $duration seconds"

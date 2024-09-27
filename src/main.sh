92732e5fcff7db7ef61500630fc3704ab379a1e4
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-27 15:15:54 -0400

    Created 'time_tracker'

--- src/main.sh
+++ src/main.sh
@@ -2,19 +2,29 @@
 
 start=$SECONDS
 
-file_access () {
+file_modification () {
     printf "\nEntering...\n\n"
     sleep 1
-    printf "Which file do you want to check the access time of? Type * to check the access time of all files.\n"
-    read input
-    
+    printf "Which file do you want to check the modification time of? Type 'All' to check the modification time of all files.\n"
+    read m_input
+
+    if [[ "$m_input" = "All" ]]; then
+        printf "Checking...\n\n"
+        sleep 1 
+        files=$(( stat -c %n %w * | sort ))
+        printf "Files:\n\n$files"
+    else 
+        printf "That is not a valid response, please try again."
+        file_modification
+    fi
 }
+
 
 file_read () {
     printf "Entering...\n\n"
     sleep 1 
     printf "Which file do you want to check the read time of? Type * to check the read time of all files.\n"
-    read input
+    read r_input
 }
 
 set_timer () {
@@ -23,7 +33,7 @@ set_timer () {
     printf "Start timer on this folder or on a specifc file?\n\n"
     printf "(1) Folder\n\n"
     printf "(2) File\n\n"
-    read input
+    read t_input
 }
 
 set_alarm () {
@@ -32,40 +42,42 @@ set_alarm () {
     printf " Set alarm on this folder or on a specific file?\n\n"
      printf "(1) Folder\n\n"
     printf "(2) File\n\n"
-    read input
+    read al_input
 }
 
 printf "\nWelcome to Time Tracker!\n\nOptions List:\n\n"
 
-options_list () {
-printf "(1) Get file access time:\n\n"
-printf "(2) Get file read time:\n\n"
-printf "(3) Set Timer:\n\n"
-printf "(4) Set Alarm:\n\n"
-printf "(5) Exit:\n\n"
-read input
 
-if [[ "$input" == 1 ]]; then
-    file_access
-elif [[ "$input" == 2 ]]; then
-    file_read
-elif [[ "$input" == 3 ]]; then
-    set_timer
-elif [[ "$input" == 4 ]]; then
-    set_alarm
-elif [[ "$input" == 5 ]]; then
-    printf "\nExiting Time Tracker..."
-    sleep 1
-    exit 0
-else 
-    printf "\nThat is not a valid response, please try again.\n\n"
-    options_list
-fi
-return 0
+
+options_list () {
+    printf "(1) Get file modification time:\n\n"
+    printf "(2) Get file read time:\n\n"
+    printf "(3) Set Timer:\n\n"
+    printf "(4) Set Alarm:\n\n"
+    printf "(5) Exit:\n\n"
+    read o_input
+    
+    if [[ "$o_input" == 1 ]]; then
+        file_modification
+    elif [[ "$o_input" == 2 ]]; then
+        file_read
+    elif [[ "$o_input" == 3 ]]; then
+        set_timer
+    elif [[ "$o_input" == 4 ]]; then
+        set_alarm
+    elif [[ "$o_input" == 5 ]]; then
+        printf "\nExiting Time Tracker..."
+        sleep 1
+        exit 0
+    else 
+        printf "\nThat is not a valid response, please try again.\n\n"
+        options_list
+    fi
+    return 0
 }
 options_list
 
-printf "\nExit Status: $?"
+printf "\nExit Status: $?\n"
 duration=$(( SECONDS - start ))
 printf "Duration: $duration seconds"
 
---
+++ time_tracker/src/main.rs
@@ -0,0 +1,18 @@
+use text_io::read;
+
+fn main() {
+ 
+}
+
+fn options_list() {
+    println!("Welcome to my Time Tracker!");
+    println!("Options List:\n\n");
+
+    println!("(1) Get Modification Time");
+    println!("(2) Get Access Time");
+    println!("(3) Set Timer");
+    println!("(4) Set Alarm");
+
+    
+
+}

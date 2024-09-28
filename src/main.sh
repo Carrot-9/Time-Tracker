b2471641a0806a7d67c34565570fdff018b465a3
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-28 01:12:25 -0400

    Added Timer and Alarm Options

--- src/main.sh
+++ src/main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-set -e
+set -x
+
+exec 5> debug_output.txt
+BASH_XTRACEFD="5"
 
 file_creation () {
     printf "\nChecking...\n\n"
     sleep 1 
     files=$( stat -c "%n %w" * | sort )
     printf "File Creation Times:\n\n$files"
-    options_list
+    options_list 
 }
 
 file_access() {
@@ -20,21 +23,52 @@ file_access() {
     options_list
 }
 
-set_timer() {
-    printf "\nChecking..\n\n"
+set_alarm() {
+    printf "\nChecking...\n\n"
     sleep 1
-    printf "How long do you want to set a timer for? Answer in seconds.\n" 
-    read t_input
-    t_input=$1
-    [[ $1 =~ "^[0-9]+$" ]] && { printf "That is a number"; exit 0;} || { printf "That is not a number, please try again"; set_timer; }
+    printf "How long do you want to set the alarm for? Answer in seconds.\n" 
+    read a_input
+    if  [[ "$a_input" =~ ^[0-9]+$ ]]; then
+        while [[ "$a_input" -gt 0 ]]; 
+        do
+            sleep 1 
+            (( a_input -= 1 ))
+        done
+        printf "\nTime is up!\n\n"
+        printf "Time is up!\n\n"
+        printf "Time is up!\n\n"
+    else 
+        printf  " \nThat is not a valid response."
+        options_list
+    fi
 }
+
+set_timer() {
+    printf "\nChecking...\n\n"
+    sleep 1 
+    printf "Starting Timer...\n\n"
+    printf "Press Ctrl + C to stop timer.\n\n"
+    count=0
+    while : ;
+    do
+        sleep 1 
+        ((count+=1))
+    done
+}
+
+ctrl_c() {
+    printf "Timer: $count seconds"
+    exit 0
+}
+trap ctrl_c INT
 
 printf "\nWelcome to Time Tracker!\n\nOptions List:\n"
 
 options_list () {
     printf "\n\n(1) Get file creation times:\n\n"
     printf "(2) Get file access and modification times:\n\n"
-    printf "(3) Set Timer:\n\n"
+    printf "(3) Set Alarm:\n\n"
+    printf "(4) Set Timer:\n\n"
     printf "(5) Exit:\n\n"
     read o_input
     
@@ -43,9 +77,9 @@ options_list () {
     elif [[ "$o_input" == 2 ]]; then
         file_access
     elif [[ "$o_input" == 3 ]]; then
-        set_timer
-    elif [[ "$o_input" == 4 ]]; then
         set_alarm
+    elif [[ "$o_input" == 4 ]]; then
+        set_timer
     elif [[ "$o_input" == 5 ]]; then
         printf "\nExiting Time Tracker..."
         sleep 1
@@ -54,7 +88,6 @@ options_list () {
         printf "\nThat is not a valid response, please try again.\n\n"
         options_list
     fi
-    return 0
 }
 options_list
 

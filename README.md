a730c730bb5b0daec838ea6b7403746c7b0dcc25
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-28 14:25:05 -0400

    Deleted 'file_modification()' and 'file_access()'

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Features
 
-Allows you to manually set and stop timers as well as create alarms.
+A simple timer written in Bash.
+
+Allows you to get various pieces of metadata such as file access and modification times.
+
+Allows to manually set and stop timers as well as create alarms.
+
+Run using the command "timer".
+
+This project was created solely to learn Bash and will therefore not be sharable. Consider this repo read-only. 
--- src/main.sh
+++ src/main.sh
@@ -1,48 +1,5 @@
 #!/bin/bash
 
-set -x
-
-exec 5> debug_output.txt
-BASH_XTRACEFD="5"
-
-file_creation () {
-    printf "\nChecking...\n\n"
-    sleep 1 
-    files=$( stat -c "%n %w" * | sort )
-    printf "File Creation Times:\n\n$files"
-    options_list 
-}
-
-file_access() {
-    printf "\nChecking...\n\n"
-    sleep 1 
-    a_files=$( stat -c "%n %x" * | sort )
-    printf "File Access Times:\n\n$a_files"
-    m_files=$( stat -c "%n %y" * | sort)
-    printf "\n\nFile Modification Times:\n\n$m_files"
-    options_list
-}
-
-set_alarm() {
-    printf "\nChecking...\n\n"
-    sleep 1
-    printf "How long do you want to set the alarm for? Answer in seconds.\n" 
-    read a_input
-    if  [[ "$a_input" =~ ^[0-9]+$ ]]; then
-        while [[ "$a_input" -gt 0 ]]; 
-        do
-            sleep 1 
-            (( a_input -= 1 ))
-        done
-        printf "\nTime is up!\n\n"
-        printf "Time is up!\n\n"
-        printf "Time is up!\n\n"
-    else 
-        printf  " \nThat is not a valid response."
-        options_list
-    fi
-}
-
 set_timer() {
     printf "\nChecking...\n\n"
     sleep 1 
@@ -62,25 +19,37 @@ ctrl_c() {
 }
 trap ctrl_c INT
 
+set_alarm() {
+    printf "\nChecking...\n\n"
+    sleep 1
+    printf "How long do you want to set the alarm for? Answer in seconds.\n" 
+    read a_input
+    if  [[ "$a_input" =~ ^[0-9]+$ ]]; then
+        while [[ "$a_input" -gt 0 ]]; 
+        do
+            sleep 1 
+            (( a_input -= 1 ))
+        done
+        printf "\nTime is up!\n"
+    else 
+        printf  " \nThat is not a valid response."
+        options_list
+    fi
+}
+
 printf "\nWelcome to Time Tracker!\n\nOptions List:\n"
 
 options_list () {
-    printf "\n\n(1) Get file creation times:\n\n"
-    printf "(2) Get file access and modification times:\n\n"
-    printf "(3) Set Alarm:\n\n"
-    printf "(4) Set Timer:\n\n"
-    printf "(5) Exit:\n\n"
+    printf "\n(1) Set Timer:\n\n"
+    printf "(2) Set Alarm:\n\n"
+    printf "(3) Exit:\n\n"
     read o_input
     
     if [[ "$o_input" == 1 ]]; then
-        file_creation
-    elif [[ "$o_input" == 2 ]]; then
-        file_access
-    elif [[ "$o_input" == 3 ]]; then
-        set_alarm
-    elif [[ "$o_input" == 4 ]]; then
         set_timer
-    elif [[ "$o_input" == 5 ]]; then
+    elif [[ "$o_input" == 2 ]]; then
+        set_alarm
+    elif [[ "$o_input" == 3 ]]; then
         printf "\nExiting Time Tracker..."
         sleep 1
         exit 0
